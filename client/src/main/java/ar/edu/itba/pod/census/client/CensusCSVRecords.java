0e4ab4000de98bb799eeb34d7f16a154e113de35
Juan <jpmrno@gmail.com>
2017-10-26 13:05:52 -0300

    add args from system properties && update csv records parser && rename habitant -> citizen

--- client/src/main/java/ar/edu/itba/pod/census/client/CensusCSVRecords.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/CensusCSVRecords.java
@@ -1,44 +1,30 @@
 package ar.edu.itba.pod.census.client;
 
+import java.io.Closeable;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Iterator;
+import java.util.function.Consumer;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class CensusCSVRecords implements Iterator<CSVRecord> {
+public final class CensusCSVRecords implements Iterator<CSVRecord>, Closeable, AutoCloseable {
 
-  private final static Logger logger = LoggerFactory.getLogger(CensusCSVRecords.class);
+  private final static Logger LOGGER = LoggerFactory.getLogger(CensusCSVRecords.class);
 
-  private final static String RESOURCES_PATH = System.getProperty("user.dir")
-      + "/client/src/main/resources/";
+  private final Reader inFile;
+  private final CSVParser parser;
+  private final Iterator<CSVRecord> recordsIterator;
 
-  private final String csvPath;
-  private Iterator<CSVRecord> recordsIterator;
+  /* package-private */ CensusCSVRecords(final String csvFilePath) throws IOException {
 
-  /* package-private */
-  CensusCSVRecords(final String csvFileName) {
-    csvPath = RESOURCES_PATH + csvFileName;
-    parseFile();
-  }
-
-  private void parseFile() {
-    try (FileReader in = new FileReader(csvPath)) {
-      parseCSVFileReader(in);
-    } catch (final IOException e) {
-      logger.warn("An IOException occurred while attempting to read CSV file: {}", csvPath);
-    }
-  }
-
-  private void parseCSVFileReader(final FileReader in) {
-    try (CSVParser parser = CSVFormat.DEFAULT.parse(in)) {
-      recordsIterator = parser.getRecords().iterator();
-    } catch (final IOException e) {
-      logger.warn("An IOException occurred while parsing the CSV file: {}", csvPath);
-    }
+    this.inFile = new FileReader(csvFilePath);
+    this.parser = CSVFormat.DEFAULT.withHeader(Headers.class).parse(inFile);
+    this.recordsIterator = parser.iterator();
   }
 
   @Override
@@ -49,5 +35,25 @@ public final class CensusCSVRecords implements Iterator<CSVRecord> {
   @Override
   public CSVRecord next() {
     return recordsIterator.next();
+  }
+
+  @Override
+  public void remove() {
+    recordsIterator.remove();
+  }
+
+  @Override
+  public void forEachRemaining(final Consumer<? super CSVRecord> action) {
+    recordsIterator.forEachRemaining(action);
+  }
+
+  @Override
+  public void close() throws IOException {
+    parser.close();
+    inFile.close();
+  }
+
+  public enum Headers {
+    EMPLOYMENT_STATUS, HOME_ID, DEPARTMENT_NAME, PROVINCE_NAME
   }
 }
--- client/src/main/java/ar/edu/itba/pod/census/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/Client.java
@@ -1,61 +1,83 @@
 package ar.edu.itba.pod.census.client;
 
-import ar.edu.itba.pod.census.ProvinceHabitant;
+import ar.edu.itba.pod.census.ProvinceCitizen;
 import ar.edu.itba.pod.census.SharedConfiguration;
+import ar.edu.itba.pod.census.client.CensusCSVRecords.Headers;
 import ar.edu.itba.pod.census.client.args.ClientArgs;
 import com.beust.jcommander.JCommander;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Client {
 
-  private final static Logger logger = LoggerFactory.getLogger(Client.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Client.class);
 
-  private final static ClientArgs clientArgs = ClientArgs.getInstance();
-  private final static HazelcastInstance hazelcast = createHazelcastClient();
+  private static final ClientArgs CLIENT_ARGS = ClientArgs.getInstance();
+
+  private static final HazelcastInstance HAZELCAST_CLIENT = createHazelcastClient();
 
   public static void main(final String[] args) {
     parseClientArguments(args);
-    loadCSVFileInHazelcastMap();
+    loadCSTFileAndPopulateHazelcastMap(CLIENT_ARGS.getInPath(),
+        Client::populateHazelcastMapWithRecords);
   }
 
   private static void parseClientArguments(final String[] args) {
-    JCommander.newBuilder().addObject(clientArgs).build().parse(args);
+    JCommander.newBuilder()
+        .addObject(CLIENT_ARGS)
+        .defaultProvider(ClientArgs.SYSTEM_PROPERTIES_PROVIDER)
+        .build()
+        .parse(args);
   }
 
-  private static void loadCSVFileInHazelcastMap() {
-    final Iterator<CSVRecord> csvRecords = new CensusCSVRecords(clientArgs.getInPath());
-    populateMapWithRecords(csvRecords);
+  private static void loadCSTFileAndPopulateHazelcastMap(final String path,
+      final Consumer<Iterator<CSVRecord>> consumer) {
+    try (final CensusCSVRecords csvRecords = new CensusCSVRecords(path)) {
+      consumer.accept(csvRecords);
+    } catch (final IOException exception) {
+      System.err.println("There was an error while trying to read the input file");
+      LOGGER.error("Could not open/read input file", exception);
+      System.exit(1);
+    }
   }
 
-  private static void populateMapWithRecords(final Iterator<CSVRecord> csvRecords) {
-    final Map<String, List<ProvinceHabitant>> map = hazelcast
-        .getMap(SharedConfiguration.getMapName());
+  // Query dependant
+  private static void populateHazelcastMapWithRecords(final Iterator<CSVRecord> csvRecords) {
+    final Map<String, List<ProvinceCitizen>> map =
+        HAZELCAST_CLIENT.getMap(SharedConfiguration.getMapName());
 
     csvRecords.forEachRemaining(record -> {
-      final ProvinceHabitant habitantToAdd = new ProvinceHabitant(Integer.parseInt(record.get(0)),
-          Integer.parseInt(record.get(1)), record.get(2));
-      map.computeIfAbsent(record.get(3), province -> new ArrayList<>()).add(habitantToAdd);
+      final ProvinceCitizen citizenToAdd = new ProvinceCitizen(
+          Integer.parseInt(record.get(Headers.EMPLOYMENT_STATUS)),
+          Integer.parseInt(record.get(Headers.HOME_ID)),
+          record.get(Headers.DEPARTMENT_NAME));
+
+      map.computeIfAbsent(record.get(Headers.PROVINCE_NAME), province -> new ArrayList<>())
+          .add(citizenToAdd);
     });
   }
 
   private static HazelcastInstance createHazelcastClient() {
-    final ClientConfig clientConfig = createClientConfigWithCredentials();
-    return HazelcastClient.newHazelcastClient(clientConfig);
+    return HazelcastClient.newHazelcastClient(createClientConfigWithCredentials());
   }
 
   private static ClientConfig createClientConfigWithCredentials() {
     final ClientConfig clientConfig = new ClientConfig();
-    clientConfig.getGroupConfig().setName(SharedConfiguration.getGroupUsername())
+
+    clientConfig.getGroupConfig()
+        .setName(SharedConfiguration.getGroupUsername())
         .setPassword(SharedConfiguration.getGroupPassword());
+
     return clientConfig;
   }
 }
