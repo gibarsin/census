6c9d5cc9b7e770e76b0e7aa3067d14acd348fac0
Juan <jpmrno@gmail.com>
2017-10-26 13:00:10 -0300

    fix maven verify && add missing run-client script && format Server

--- server/src/main/java/ar/edu/itba/pod/census/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/census/server/Server.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) {
-        logger.info("census Server Starting ...");
-    }
+  private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+  public static void main(String[] args) {
+    logger.info("census Server Starting ...");
+  }
 }
