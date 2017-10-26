6bcc3b0a2d8e30b5f15005765bd0bbe0359f2948
Juan <jpmrno@gmail.com>
2017-10-26 19:07:41 -0300

    add missing pom deps && add hazelcast server

--- server/src/main/java/ar/edu/itba/pod/census/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/census/server/Server.java
@@ -1,13 +1,32 @@
 package ar.edu.itba.pod.census.server;
 
+import ar.edu.itba.pod.census.config.SharedConfiguration;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Server {
+public final class Server {
 
-  private static Logger logger = LoggerFactory.getLogger(Server.class);
+  private static Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+  private Server() {
+  }
 
   public static void main(String[] args) {
-    logger.info("census Server Starting ...");
+    LOGGER.debug("Server starting...");
+    final HazelcastInstance hazelcastServer = createHazelcastServer();
+    LOGGER.info("Server started");
+  }
+
+  private static HazelcastInstance createHazelcastServer() {
+    final Config serverConfig = new Config();
+
+    serverConfig.getGroupConfig()
+        .setName(SharedConfiguration.GROUP_USERNAME)
+        .setPassword(SharedConfiguration.GROUP_PASSWORD);
+
+    return Hazelcast.newHazelcastInstance(serverConfig);
   }
 }
