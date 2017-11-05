892d9ce3bce8a7e62f1e6ed2644ef300d0928657
Matias Comercio <mcomerciovazquez@gmail.com>
2017-11-05 18:38:36 -0300

    Remove unused code

--- client/src/main/java/ar/edu/itba/pod/census/client/CensusCSVRecords.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/CensusCSVRecords.java
@@ -1,52 +1,6 @@
 package ar.edu.itba.pod.census.client;
 
-import java.io.Closeable;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
-public final class CensusCSVRecords implements Iterator<CSVRecord>, Closeable, AutoCloseable {
-  private final Reader inFile;
-  private final CSVParser parser;
-  private final Iterator<CSVRecord> recordsIterator;
-
-  private CensusCSVRecords(final String csvFilePath) throws IOException {
-    this.inFile = new FileReader(csvFilePath);
-    this.parser = CSVFormat.DEFAULT.withHeader(Headers.class).parse(inFile);
-    this.recordsIterator = parser.iterator();
-  }
-
-  public static CensusCSVRecords open(final String csvFilePath) throws IOException {
-    return new CensusCSVRecords(csvFilePath);
-  }
-
-  @Override
-  public boolean hasNext() {
-    return recordsIterator.hasNext();
-  }
-
-  @Override
-  public CSVRecord next() {
-    return recordsIterator.next();
-  }
-
-  @Override
-  public void remove() {
-    recordsIterator.remove();
-  }
-
-  @Override
-  public void close() throws IOException {
-    parser.close();
-    inFile.close();
-  }
-
-  // IMPORTANT: Do not alter the order of the headers values
-  // TODO: Migrate this to indexes, remove the above behavior and leave only this enum
+public class CensusCSVRecords {
   public enum Headers {
     EMPLOYMENT_STATUS(0), HOME_ID(1), DEPARTMENT_NAME(2), PROVINCE_NAME(3);
 
