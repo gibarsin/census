0367f0c8d7d60163b1b02e93db2084454281af06
Matias Comercio <mcomerciovazquez@gmail.com>
2017-11-05 12:48:59 -0300

    Migrate Query 1

--- client/src/main/java/ar/edu/itba/pod/census/client/query/RegionPopulationQuery.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/query/RegionPopulationQuery.java
@@ -8,13 +8,14 @@ import ar.edu.itba.pod.census.config.SharedConfiguration;
 import ar.edu.itba.pod.census.mapper.RegionPopulationMapper;
 import ar.edu.itba.pod.census.reducer.NoKeyAdderReducerFactory;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
+import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.*;
 
 import java.io.PrintStream;
 import java.util.Collections;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ExecutionException;
 
@@ -22,9 +23,10 @@ import java.util.concurrent.ExecutionException;
 // Intentionally as we are using deprecated Hazelcast features
 public final class RegionPopulationQuery extends AbstractQuery {
 
-  private List<String> localInput = new LinkedList<>();
-  private IList<String> remoteInput;
-  private ReducingSubmittableJob<String, String, Integer> mapReducerJob;
+  private int key;
+  private Map<Integer, String> localInput;
+  private IMap<Integer, String> remoteInput;
+  private ReducingSubmittableJob<Integer, String, Integer> mapReducerJob;
   private Collator<Entry<String, Integer>, List<Entry<String, Integer>>> collator;
   private List<Entry<String, Integer>> jobResult;
 
@@ -34,25 +36,27 @@ public final class RegionPopulationQuery extends AbstractQuery {
 
   @Override
   protected void pickAClearClusterCollection(final HazelcastInstance hazelcastInstance) {
-    remoteInput = hazelcastInstance.getList(SharedConfiguration.STRUCTURE_NAME);
+    localInput = new HashMap<>();
+    key = 0;
+    remoteInput = hazelcastInstance.getMap(SharedConfiguration.STRUCTURE_NAME);
     remoteInput.clear();
   }
 
   @Override
   protected void addRecordToClusterCollection(final String[] csvRecord) {
-    localInput.add(csvRecord[Headers.PROVINCE_NAME.getColumn()]);
+    localInput.put(key ++, csvRecord[Headers.PROVINCE_NAME.getColumn()]);
   }
 
   @Override
   protected void submitAllRecordsToCluster() {
-    remoteInput.addAll(localInput);
+    remoteInput.putAll(localInput);
   }
 
   @Override
   protected void prepareJobResources(final JobTracker jobTracker) {
     // Create the custom job
-    final KeyValueSource<String, String> source = KeyValueSource.fromList(remoteInput);
-    final Job<String, String> job = jobTracker.newJob(source);
+    final KeyValueSource<Integer, String> source = KeyValueSource.fromMap(remoteInput);
+    final Job<Integer, String> job = jobTracker.newJob(source);
 
     // Prepare the map reduce job to be submitted
     mapReducerJob = job.mapper(new RegionPopulationMapper())
