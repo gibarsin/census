df9e2de32c27b7876d422769f70dcfaadd37b179
Matias Comercio <mcomerciovazquez@gmail.com>
2017-11-05 21:14:54 -0300

    Hazelcast file is included in compilation time

--- server/src/main/java/ar/edu/itba/pod/census/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/census/server/Server.java
@@ -4,31 +4,30 @@ import ar.edu.itba.pod.census.model.SerialDataFactory;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
+
 import java.io.FileNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Server {
-
-  private static Logger LOGGER = LoggerFactory.getLogger(Server.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
   private Server() {
   }
 
   public static void main(String[] args) throws FileNotFoundException {
     LOGGER.debug("Server starting...");
-    final HazelcastInstance hazelcastServer = createHazelcastServer();
+    createHazelcastServer();
     LOGGER.info("Server started");
   }
 
-  private static HazelcastInstance createHazelcastServer() throws FileNotFoundException {
+  private static void createHazelcastServer() throws FileNotFoundException {
     final Config serverConfig = new XmlConfigBuilder(
         System.getProperty("user.dir") + "/hazelcast.xml")
         .build();
     serverConfig.getSerializationConfig()
             .addDataSerializableFactory(SerialDataFactory.FACTORY_ID, new SerialDataFactory());
 
-    return Hazelcast.newHazelcastInstance(serverConfig);
+    Hazelcast.newHazelcastInstance(serverConfig);
   }
 }
